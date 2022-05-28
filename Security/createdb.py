909e794dfaa3dcd9bdcc973b81608ce992cad944
Melek Elloumi <melek.elloumi@insat.ucar.tn>
2022-05-28 11:35:02 +0100

    security app done

--- Security/createdb.py
+++ Security/createdb.py
@@ -1,5 +1,3 @@
-import sqlite3
-
 from pymongo import MongoClient
 
 def initiate_database():
@@ -23,18 +21,5 @@ def initiate_database():
     ]
     col.insert_many(data)
 
-def create_db():
-    connection = sqlite3.connect('database.db')
-    cur = connection.cursor()
-    cur.execute("DROP TABLE IF EXISTS User")
-    cur.execute(
-        "CREATE TABLE User (ID INTEGER PRIMARY KEY, USERNAME TEXT NOT NULL, PASSWORD TEXT NOT NULL);")
-    cur.execute(
-        "INSERT INTO User (USERNAME,PASSWORD) VALUES ('admin','admin'),('root','root'),('melek','elloumi');")
-    connection.commit()
-    print("Done")
-    connection.close()
-
 if __name__ == '__main__':
-    #create_db()
     initiate_database()
---
+++ Security/securityapp.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+from pymongo import MongoClient
+
+def create_app(name):
+
+    app = Flask(name)
+
+    @app.route('/login', methods=['GET'])
+    def login():
+        username = request.args.get('username', default="none", type=str)
+        password = request.args.get('password', default='none', type=str)
+        CONNECTION_STRING = "mongodb://localhost:27017"
+        client = MongoClient(CONNECTION_STRING)
+        db = client["Architecture_MS_Security"]
+        col = db["Users"]
+        user=col.find_one({"username":username})
+        response={}
+        if (user!=None):
+            if password==user["password"]:
+                response["loggedin"]=True
+                response["message"] = "Logged in successfully"
+            else:
+                response["loggedin"]=False
+                response["error"] = "Uncorrect password"
+        else:
+            response["loggedin"]=False
+            response["error"] = "User not found"
+        return response
+
+    return app
+
+
+
+def main( ):
+    app = create_app(__name__)
+    app.secret_key = 'secret123'
+    app.run(port=5000)
+
+if __name__ == '__main__':
+    main()
